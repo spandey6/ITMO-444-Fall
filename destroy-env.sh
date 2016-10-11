06eb0e5231353a379420ab114a6bbb2accb0ad8d
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-10-11 21:35:58 +0000

    Made changes on query and sleep part

--- destroy-env.sh
+++ destroy-env.sh
@@ -2,9 +2,8 @@
 
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name ITMO444-hw4 --min-size 0 --max-size 0 --desired-capacity 0
 
-ID=aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone,State.Name,InstanceId]' | grep -E 'running|pending'| awk '{print $5}'
-
-#De-register instances
+# De-register the instances form the load balancer
+ID=aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone,State.Name,InstanceId]' | grep us-west-2b |grep running| grep pending| awk '{print $3}'
 #aws elb deregister-instances-from-load-balancer --load-balancer-name ITMO-444-sudu --instances $ID
 echo "De-registring the load balancer please wait....."
 sleep 10
@@ -13,7 +12,7 @@ echo "Load balancer is de-registered."
 # Detach autoscaling load balancer
 aws autoscaling detach-load-balancers --load-balancer-names ITMO-444-sudu --auto-scaling-group-name ITMO444-hw4
 echo "Detaching load blalancer please wait......."
-sleep 50
+sleep 70
 echo "Load balancer is detached"
 
 # Deleting autoscaling group
