3dcd198066d04387ed36d6919f91b3ce71c20a98
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-10-15 21:17:51 +0000

    Few changes made on positional parameters

--- create-env.sh
+++ create-env.sh
@@ -13,15 +13,17 @@ count=$5
 #Create Load balancer
 subnet = aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'
 
-aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnet $subnet --security-groups $securitygroup
+aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --client-token sudu --instance-type t2.micro --user-data file://installapp.sh --placement AvailabilityZone=us-west-2b --count $5
+
+aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnet $subnet --security-groups $3
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
-aws autoscaling create-launch-configuration --launch-configuration-name $launchconfiguration --image-id $amiid --security-groups $securitygroup --key-name $keyname --instance-type t2.micro --user-data file://installenv.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $amiid --security-groups $3 --key-name $2 --instance-type t2.micro --user-data file://installenv.sh
 echo "Launch configuration is created."
 
 # Creating autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444-hw4 --launch-configuration $launchconfiguration --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 2 --desired-capacity $count
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444-hw4 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 2 --desired-capacity $5
 echo "Auto-scaling group is created."
 
 # Attaching the load balancer to the group
