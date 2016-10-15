78ba061313533a5d7d981c887f43c69b121abd5f
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-10-15 21:00:27 +0000

    Added the positional parameters in the create-env.sh file

--- create-env.sh
+++ create-env.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+
+if [ $# != 5 ]
+then
+echo "There are less than 5 parameters AMI ID, key-name, security-group, launch configuration, and count."
+ else
+amiid=$1
+keyname=$2
+securitygroup=$3
+launchconfiguration=$4
+count=$5
+
 #Create Load balancer
-aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets subnet-a8dcaede --security-groups sg-0e14c377
+subnet = aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'
+
+aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnet $subnet --security-groups $securitygroup
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
-aws autoscaling create-launch-configuration --launch-configuration-name ITMO-444-hw4 --image-id ami-06b94666 --security-groups sg-0e14c377 --key-name spandey --instance-type t2.micro --user-data file://installenv.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $launchconfiguration --image-id $amiid --security-groups $securitygroup --key-name $keyname --instance-type t2.micro --user-data file://installenv.sh
 echo "Launch configuration is created."
 
 # Creating autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444-hw4 --launch-configuration ITMO-444-hw4 --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 2 --desired-capacity 3
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444-hw4 --launch-configuration $launchconfiguration --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 2 --desired-capacity $count
 echo "Auto-scaling group is created."
 
 # Attaching the load balancer to the group
 aws autoscaling attach-load-balancers --load-balancer-names ITMO-444-sudu --auto-scaling-group-name ITMO444-hw4
 echo "Done"
+fi
