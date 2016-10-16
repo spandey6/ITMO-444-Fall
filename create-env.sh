2c7276d78a889a89cb1827d3bae2784eac2d4e89
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-10-16 03:48:31 +0000

    Added the parameters and the comments.

--- create-env.sh
+++ create-env.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-if [ $# != 5 ]
+if ["$#" -ne 5];
 then
-echo "There are less than 5 parameters AMI ID, key-name, security-group, launch configuration, and count."
- else
-amiid=$1
-keyname=$2
-securitygroup=$3
-launchconfiguration=$4
+echo "Parameters are missing"
+echo-n "The parameter should be in a format showen below."
+echo-n "1.AMI ID:ami-06b94666"
+echo-n "2.KEY-NAME:spandey"
+echo-n "3.SECURITY-GROUP: sg-0e14c377"
+echo-n "4.LAUNCH-CONFIGURATION: ITMO-444-hw4"
+echo-n "5.COUNT: Number os instances where minimum is 2 and maximum is 5"
+exit 0
+else
+echo "All the parameters are preseent."
+amiid=ami-06b94666=$1
+keyname=spandey=$2
+securitygroup=sg-0e14c377=$3
+launchconfiguration=ITMO-444-hw4=$4
 count=$5
 
+#VPCID= '(aws ec2 create-vpc -- cidr-block 10.0.0.0/28 --output=text | ('print $6')')
+
 #Create Load balancer
-subnet = aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'
+SUBNET = aws ec2 describe-subnets --filters "Name=availabilityZone,Values=us-west-2b" --query 'Subnets[*].SubnetId'
 
-aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --client-token sudu --instance-type t2.micro --user-data file://installapp.sh --placement AvailabilityZone=us-west-2b --count $5
+aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --client-token sudu --instance-type t2.micro --user-data file://installenv.sh --placement AvailabilityZone=us-west-2b --count $5
 
-aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnet $subnet --security-groups $3
+aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnet $SUBNET --security-groups $3
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
