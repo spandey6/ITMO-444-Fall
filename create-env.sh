a4238385aa0eb5b19d32439f39165b724dc71331
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-10-28 02:18:33 +0000

    Fixed small typo mistakes in the  code.

--- create-env.sh
+++ create-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [$# != 5 ]; then
-#echo "Parameters are missing"
+if [ $# != 5 ]; then
+echo "Parameters are missing"
 #echo-n "The parameter should be in a format showen below."
 #echo-n "1.AMI ID:ami-06b94666"
 #echo-n "2.KEY-NAME:spandey"
@@ -14,7 +14,7 @@ echo "All the parameters are preseent."
 fi
 
 #Create Load balancer
-aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80
+aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
@@ -22,6 +22,6 @@ aws autoscaling create-launch-configuration --launch-configuration-name $4 --ima
 echo "Launch configuration is created."
 
 # Creating autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444-hw4 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 1 --desired-capacity $5
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 1 --desired-capacity $5
 echo "Auto-scaling group is created."
 echo "Done"
