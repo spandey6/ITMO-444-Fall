f494dfa80f4b2fd0d1a57f3a730de83e2b6f4143
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-06 15:42:37 +0000

    Added parameters for load balancer

--- create-env.sh
+++ create-env.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if [ $# != 5 ]; then
+if [ $# != 7 ]; then
 echo "Parameters are missing"
 #echo-n "The parameter should be in a format showen below."
-#echo-n "1.AMI ID:ami-06b94666"
+#echo-n "1.AMI ID:ami-ami-1fed4c7f"
 #echo-n "2.KEY-NAME:spandey"
 #echo-n "3.SECURITY-GROUP: sg-0e14c377"
 #echo-n "4.LAUNCH-CONFIGURATION: ITMO-444-hw4"
-#echo-n "5.COUNT: Number os instances where minimum is 2 and maximum is 5"
+#echo-n "5.COUNT: Number os instances
 exit 0
 else
 echo "All the parameters are preseent."
 fi
 
 # Run instances 
-aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --count $5 --user-data file://installenv.sh
+aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --count $1 --user-data file://installenv.sh
 wait
 echo "Instance created"
 
 #Create Load balancer
-aws elb create-load-balancer --load-balancer-name ITMO-444-sudu --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
+aws elb create-load-balancer --load-balancer-name $6 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
@@ -27,6 +27,14 @@ aws autoscaling create-launch-configuration --launch-configuration-name $4 --ima
 echo "Launch configuration is created."
 
 # Creating autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name ITMO444 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name ITMO-444-sudu --max-size 5 --min-size 1 --desired-capacity $5
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name $7 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name $6 --max-size 5 --min-size 1 --desired-capacity $5
 echo "Auto-scaling group is created."
+
+#creting sns topic
+aws sns create-topic --name sudu
+echo "sns topic created"
+
+#Subscribing sns topic
+aws sns subscribe --topic -arn arn:aws:sns:us-west-2:839071323477:sudu --protocol sms --notification-endpoint 7735171580
+
 echo "Done"
