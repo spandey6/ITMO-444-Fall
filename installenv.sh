f354c30dc3babd49004774f859ab82e799f7b317
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-06 18:23:26 +0000

    Deleted the php installation

--- installenv.sh
+++ installenv.sh
@@ -7,8 +7,5 @@ sudo apt-get -y install apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
-sudo apt-get install -y php-xm php php-mysql curl php-curl zip unzip git
-
-
 cd /home/ubuntu/spandey6
-git clone git@github.com:illonois-itm/spandey6.git
+#git clone git@github.com:illonois-itm/spandey6.git
