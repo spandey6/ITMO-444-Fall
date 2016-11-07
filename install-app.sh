405bfe37c243938e2023b9e087e18b938759a44c
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-07 02:29:03 +0000

    Added php composer

--- install-app.sh
+++ install-app.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install -y php-xm php php-mysql curl php-curl zip unzip git
+sudo apt-get update -y
+sudo clone git clone git@github.com:illonois-itm/spandey6.git
+sudo apt-get install -y php-xm php5 apache2 php-mysql curl php-curl php5-curl zip unzip git
 
-export COMPOSER_HOME=/root && /usr/bin/composer.phar self-update 1.0.0-alpha11
 curl -sS https://getcomposer.org/installer | php
+
 php composer.phar require aws/aws-sdk-php
+
