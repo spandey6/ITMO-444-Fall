cdce929baf85a28d91f750aa35b348323db9b295
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-07 05:44:39 +0000

    Created sqs queue

--- create-app-env.sh
+++ create-app-env.sh
@@ -7,7 +7,17 @@ echo "Your database instance id is: "$db-id-for-instance
 
 aws rds wait db-instance-available --db-instance-identifier $db-id-for-instance
 
+#creting sns topic
+aws sns create-topic --name sudu
+echo "sns topic created"
+
+#Subscribing sns topic
+aws sns subscribe --topic -arn  --protocol sms --notification-endpoint 7735171580
+
+#Create sqs queus
+aws sqs create-queue --queue-name ITMO444 --attribites file://create-queue.text
+
 #delete db instances
-aws rds delete-db-instance --skip-final-snapshot --db-instance-identifier $db-id-for-instance
-aws rds wait db-instance-deleted --db-instance-identifier $db-id-for-instance
+#aws rds delete-db-instance --skip-final-snapshot --db-instance-identifier $db-id-for-instance
+#aws rds wait db-instance-deleted --db-instance-identifier $db-id-for-instance
 #echo "Database deleted"
