2e8705b5d63bf097dcfb83d13053396d2f892f94
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-08 19:06:53 +0000

    Testing after git problem

--- install-app.sh
+++ install-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo git clone git@github.com:illonois-itm/spandey6.git
+#sudo git clone git@github.com:illonois-itm/spandey6.git
 sudo apt-get install -y php5-xm php5 apache2 php5-mysql curl php5-curl wget zip unzip git
 sudo apt-get install -y libaoache2-mod-php
 
