6c455e24d0c59c8d57d3d221ef49532a9f93bcf3
spandey6 <spandey6@hawk.iit.edu>
2016-11-08 10:56:49 -0600

    installing changes
    added libapache and wget

--- install-app.sh
+++ install-app.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get update -y
 sudo git clone git@github.com:illonois-itm/spandey6.git
-sudo apt-get install -y php5-xm php5 apache2 php5-mysql curl php5-curl zip unzip git
+sudo apt-get install -y php5-xm php5 apache2 php5-mysql curl php5-curl wget zip unzip git
+sudo apt-get install -y libaoache2-mod-php
 
 curl -sS https://getcomposer.org/installer | php
 
