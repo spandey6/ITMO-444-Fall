7fd3aa777e10d0760257717f9c5c29774852ae02
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-11 03:34:14 +0000

    Fixed small wording

--- create-env.sh
+++ create-env.sh
@@ -18,14 +18,14 @@ aws ec2 run-instances --image-id $ami_id --key-name $key_name --security-group-i
 echo "Instance created"
 
 #Create Load balancer
-aws elb create-load-balancer --load-balancer-name $load_bal_name --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
+aws elb create-load-balancer --load-balancer-name $load_balancer --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
-aws autoscaling create-launch-configuration --launch-configuration-name $launch_conf_name --image-id $ami_id --key-name $key_name --security-groups $security_group --instance-type t2.micro --user-data file://installenv.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $launch_configuration --image-id $ami_id --key-name $key_name --security-groups $security_group --instance-type t2.micro --user-data file://installenv.sh
 echo "Launch configuration is created."
 
 # Creating autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name $autoscaling_group_name --launch-configuration $launch_conf_name --availability-zone us-west-2b --load-balancer-name $load_bal_name --max-size 5 --min-size 1 --desired-capacity 1
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name $autoscaling --launch-configuration $launch_configuration --availability-zone us-west-2b --load-balancer-name $load_balancer --max-size 5 --min-size 1 --desired-capacity 1
 echo "Auto-scaling group is created."
 echo "Done"
