2668ce3eded298540f0d48fdba85e04f8db11d4b
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-11 03:29:48 +0000

    changes some parameters name

--- create-env.sh
+++ create-env.sh
@@ -14,19 +14,18 @@ echo "All the parameters are preseent."
 fi
 
 # Run instances 
-aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --count $1 --user-data file://installenv.sh --placement AvailabilityZone=us-west-2b --iam-instance-profile Name=developer
-wait
+aws ec2 run-instances --image-id $ami_id --key-name $key_name --security-group-ids $security_group --instance-type t2.micro --count $1 --user-data file://installenv.sh --placement AvailabilityZone=us-west-2b --iam-instance-profile Name=developer
 echo "Instance created"
 
 #Create Load balancer
-aws elb create-load-balancer --load-balancer-name $6 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
+aws elb create-load-balancer --load-balancer-name $load_bal_name --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zones us-west-2b
 echo "Load Balancer is Created"
 
 # Creating autoscaling launch configuration
-aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --key-name $2 --security-groups $3 --instance-type t2.micro --user-data file://installenv.sh
+aws autoscaling create-launch-configuration --launch-configuration-name $launch_conf_name --image-id $ami_id --key-name $key_name --security-groups $security_group --instance-type t2.micro --user-data file://installenv.sh
 echo "Launch configuration is created."
 
 # Creating autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name $7 --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name $6 --max-size 5 --min-size 1 --desired-capacity $5
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name $autoscaling_group_name --launch-configuration $launch_conf_name --availability-zone us-west-2b --load-balancer-name $load_bal_name --max-size 5 --min-size 1 --desired-capacity 1
 echo "Auto-scaling group is created."
 echo "Done"
