088105d56f2654385cc23bf0a998f59691fd8589
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-21 20:39:50 +0000

    use of sample page of welcome.php

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+echo $_POST['email'] . "\n";
+
+$email = $_POST['email'];
+
+echo "Your email is: " . $email . "\n";
+
+$_SESSION['email'] = $_POST['email'];
+
+
+
+
+?>
+
+<html>
+<head><title>Hello app</title>
+</head>
+<body>
+<hr />
+<a href="gallery.php"> Gallery </a> | <a href="upload.php"> Upload </a>
+</body>
+</html>
