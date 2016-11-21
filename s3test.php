4c4513f419a855136efc51f32b4384e3afa3cd9d
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-21 20:15:41 +0000

    picture inserted

--- s3test.php
+++ s3test.php
@@ -6,6 +6,9 @@
 
  <?php
         echo "Hello World";
- ?>
+ 
+echo $image_url("https://s3-us-west-2.amazonaws.com/raw-spd/switchonarex.png");
+?>
+<img src="<?php echo $image_url>">
 </body>
-</html>
+</html
