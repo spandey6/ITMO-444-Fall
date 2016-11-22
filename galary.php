60a41793987554228af906a17a9590465f52afeb
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-11-22 16:31:59 +0000

    endpoint added

--- galary.php
+++ galary.php
@@ -7,11 +7,25 @@ $email = $_SESSION['email'];
 echo "\n" . md5($email);
 
 $_SESSION['receipt'] = md5($email);
+$rds = new Aws\Rds\RdsClient([
+'version' => 'latest',
+'region' => 'us-west-2'
+]);
+$result - $rds->describeDBInstances([
+'DBInstanceIdentifier' => '',
+]);
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address']
+
+//Database begins
+$link = mysqli_connect($endpoint, "root","letmein","spandey") or die("Error" .mysqli_error($link));
+
+//connection 
+
 ?>
 
 <html>
 <head>
-<title>Galary app</title>
+<title>Galary Page</title>
 </head>
 <body>
 Gallery
