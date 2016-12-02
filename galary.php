e6ca691671e43993b0bfaa7d6a2e8a88710743b8
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-12-02 17:46:21 +0000

    Made changes on database part and the also fixed all the usernames too

--- galary.php
+++ galary.php
@@ -1,34 +1,52 @@
+<html>
+<head> <title> galary </title> 
+</head>
+<body>
+<h1> Galary Images </h1>
+
 <?php
+require 'vendor/autoload.php';
 
+#show the images uploaded by the user
 session_start();
+$email = $_POST["email"];
+echo "Your email is: " .  $email;
 
-echo "Your email is: " .  $_SESSION['email'];
-$email = $_SESSION['email'];
-echo "\n" . md5($email);
-
-$_SESSION['receipt'] = md5($email);
 $rds = new Aws\Rds\RdsClient([
 'version' => 'latest',
 'region' => 'us-west-2'
 ]);
+
 $result - $rds->describeDBInstances([
-'DBInstanceIdentifier' => '',
+'DBInstanceIdentifier' => 'fp-spd-db',
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address']
 
 //Database begins
-$link = mysqli_connect($endpoint, "root","letmein","spandey") or die("Error" .mysqli_error($link));
+$link = mysqli_connect($endpoint, "controller","letmein","fp-spd-db") or die("Error" .mysqli_error($link));
 
-//connection 
-
+//Check connection 
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+mysqli_query($link, "SELECT * FROM comments WHERE email = '$email'");
+$results = $link->insert_id;
+$query = "SELECT * FROM comments WHERE email = '$email'";
+if($res =$link->query($query))
+{
+//Print the pictures from finished and raw buket
+}
+while ($row = $res->fetch_assoc()) {
+	if($_SESSION['gallery']({
+		echo "<img src =\" " . $row['FinishedS3'] . "\" /> <br />";
+	}
+	else {
+		echo "<img src =\" " . $row['RawS3'] . "\" /> <br />";
+	}
+}
+$link->close();
 ?>
+</body>
+</html>
 
-<html>
-<head>
-<title>Galary Page</title>
-</head>
-<body>
-Gallery
-<a href="gallery.php"> Gallery </a> <a href="upload.php"> Upload </a>
-
-</body></html>
