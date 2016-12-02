7662998aec7e8e68ce2803a48b92d0b071d585ae
Sudarshan Pandey <spandey6@hawk.iit.edu>
2016-12-02 18:26:29 +0000

    Added the username for the database instance

--- create-app-env.sh
+++ create-app-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Create db instances 
-aws rds create-db-instance --db-instance-identifier ITMO444-mysqldb-sudu --allocated-storage 10 --db-instance-class db.t2.micro --engine mysql --master-username user --master-user-password userpass
+aws rds create-db-instance --db-instance-identifier fp-spn-db --allocated-storage 10 --db-instance-class db.t2.micro --engine mysql --master-username controller --master-user-password letmein --publicly accessible
 
 db_instance='aws rds describe-db-instances --query 'DBInstances[*].DBInstanceIdentifier''
 echo "Your database instance id is: "$db_instance
@@ -22,3 +22,8 @@ aws sqs create-queue --queue-name ITMO444 --attribites file://create-queue.json
 aws s3 mb s3://raw-spd
 aws s3 mb s3://finished-spd
 aws cp --acl public-read switchonarex.png s3://raw-spd
+
+#Describe db instances
+declare -a dbinstance
+DBINSTANCEIN=(`aws rds describe-db-instances --output text | grep ENDPOINT | awk {' print $2'}`)
+echo 
